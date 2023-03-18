2d369f78a55eeec8363f4057c60df64e2b347cb9
mohammadamanpatel <125564920+mohammadamanpatel@users.noreply.github.com>
2023-03-18 18:51:41 +0530

    Create reverseOnlyLetters.cpp

---
+++ reverseOnlyLetters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+       int i = 0;
+       int j = s.length()-1;
+       while(i<j)
+       {
+           if(isalpha(s[i]) && isalpha(s[j]))
+           {
+               swap(s[i],s[j]);
+               i++;
+               j--;
+           }
+           if(!isalpha(s[i]))
+           {
+               i++;
+           }
+           if(!isalpha(s[j]))
+           {
+               j--;
+           }
+       }
+       return s;
+    }
+};
